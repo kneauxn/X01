45c61d64fd4ca5352026a26a3b8d11532bfe10a8
kneauxn <kneauxn@gmail.com>
2016-11-16 02:58:21 -0700

    The basic structure

---
+++ src/app/dartboard/dartboard.component.spec.ts
@@ -0,0 +1,28 @@
+/* tslint:disable:no-unused-variable */
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
+import { DartboardComponent } from './dartboard.component';
+
+describe('DartboardComponent', () => {
+  let component: DartboardComponent;
+  let fixture: ComponentFixture<DartboardComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DartboardComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DartboardComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/dartboard/dartboard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dartboard',
+  templateUrl: './dartboard.component.html',
+  styleUrls: ['./dartboard.component.css']
+})
+export class DartboardComponent implements OnInit {
+  title: 'Dartboard';
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
