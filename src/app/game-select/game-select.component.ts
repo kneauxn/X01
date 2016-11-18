3bc24505c09bdfda6d7bda1659adfa7a81070063
kneauxn <kneauxn@gmail.com>
2016-11-17 19:22:46 -0700

    Got some two-way functionality working on the game selector. Now if you have chosen a game type the game select component disappears

--- src/app/game-select/game-select.component.ts
+++ src/app/game-select/game-select.component.ts
@@ -7,6 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GameSelectComponent implements OnInit {
   title = 'Game Select';
+  chosenGame: number[] = [];
+
+  // The functions that change the chosenGame value
+  // 1 = X01; 2 = Cricket; 3 = Aroung the Clock
+  setGameType1(): void {
+    this.chosenGame[0] = 1;
+  }
+  setGameType2(): void {
+    this.chosenGame[0] = 2;
+  }
+  setGameType3(): void {
+    this.chosenGame[0] = 3;
+  }
+
   constructor() { }
 
   ngOnInit() {
